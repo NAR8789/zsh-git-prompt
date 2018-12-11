7a96e77564de9b79eb6f8ec41a037cfcfb0bec8a
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-11 00:11:12 -0800

    be consistent with how other ZSH_THEME_GIT_PROMPT vars are set

--- zshrc.sh
+++ zshrc.sh
@@ -150,6 +150,6 @@ ZSH_THEME_GIT_PROMPT_UPSTREAM_FRONT=" {%{$fg[blue]%}"
 ZSH_THEME_GIT_PROMPT_UPSTREAM_END="%{${reset_color}%}}"
 ZSH_THEME_GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}|MERGING%{${reset_color}%}"
 ZSH_THEME_GIT_PROMPT_REBASE="%{$fg_bold[magenta]%}|REBASE%{${reset_color}%} "
-ZSH_THEME_GIT_PROMPT=${ZSH_THEME_GIT_PROMPT:-'$ZSH_THEME_GIT_PROMPT_PREFIX$branch$merge_or_rebase$upstream${${:-$behind$ahead}:+ }$behind$ahead$ZSH_THEME_GIT_PROMPT_SEPARATOR$staged$conflicts$changed$untracked$stashed$clean$ZSH_THEME_GIT_PROMPT_SUFFIX'}
+ZSH_THEME_GIT_PROMPT='$ZSH_THEME_GIT_PROMPT_PREFIX$branch$merge_or_rebase$upstream${${:-$behind$ahead}:+ }$behind$ahead$ZSH_THEME_GIT_PROMPT_SEPARATOR$staged$conflicts$changed$untracked$stashed$clean$ZSH_THEME_GIT_PROMPT_SUFFIX'
 
 # vim: set filetype=zsh:
