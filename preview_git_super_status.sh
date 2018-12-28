29b435543e84cfbef2fe31294c2f8a03fb93ba10
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-28 15:13:39 -0800

    previewer should only modify git status vars. It should not modify theme vars

--- preview_git_super_status.sh
+++ preview_git_super_status.sh
@@ -2,19 +2,14 @@ preview_git_super_status() {
   (
     source "$__GIT_PROMPT_DIR/test/helpers/preview_git_status.zsh"
 
-    clean_env() {
-      clean_git_vars
-      unset ZSH_GIT_PROMPT_SHOW_UPSTREAM
-    }
-
     show_git_prompt() {
       print -P "$1$(git_super_status)"
     }
 
-    clean_env
+    clean_git_vars
     show_git_prompt "clean:\t\t\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_AHEAD=1
     GIT_BEHIND=2
     GIT_STAGED=3
@@ -23,11 +18,11 @@ preview_git_super_status() {
     GIT_STASHED=7
     show_git_prompt "basic metrics:\t\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_LOCAL_ONLY=1
     show_git_prompt "local only:\t\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_AHEAD=1
     GIT_BEHIND=2
     GIT_STAGED=3
@@ -37,12 +32,12 @@ preview_git_super_status() {
     GIT_LOCAL_ONLY=1
     show_git_prompt "local only, with metrics:\t"
 
-    clean_env
+    clean_git_vars
     GIT_MERGING=1
     GIT_CONFLICTS=1
     show_git_prompt "merging:\t\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_MERGING=1
     GIT_AHEAD=1
     GIT_BEHIND=2
@@ -54,12 +49,12 @@ preview_git_super_status() {
     GIT_LOCAL_ONLY=1
     show_git_prompt "merging, with metrics:\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_BRANCH=:abc1234
     GIT_REBASE=3/10
     show_git_prompt "rebasing:\t\t\t"
 
-    clean_env
+    clean_git_vars
     GIT_REBASE=2/7
     GIT_BRANCH=:abc1234
     GIT_STAGED=3
@@ -68,18 +63,6 @@ preview_git_super_status() {
     GIT_UNTRACKED=6
     GIT_STASHED=7
     show_git_prompt "rebasing, with metrics:\t\t"
-
-    clean_env
-    ZSH_GIT_PROMPT_SHOW_UPSTREAM=2
-    GIT_MERGING=1
-    GIT_AHEAD=1
-    GIT_BEHIND=2
-    GIT_STAGED=3
-    GIT_CONFLICTS=4
-    GIT_CHANGED=5
-    GIT_UNTRACKED=6
-    GIT_STASHED=7
-    show_git_prompt "merging, with show_upstream:\t"
   )
 }
 
